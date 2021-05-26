e401cbdc6ece937ec4dbb3b5f3bc4d7351396f03
PauloSGM <meiotodo@hotmail.com>
2021-05-25 21:59:14 -0300

    mostrando números na tela

---
+++ js/script.js
@@ -0,0 +1,49 @@
+
+    
+    let n2 = document.getElementById("b2").value;
+    let n3 = document.getElementById("b3");
+    let n4 = document.getElementById("b4");
+    let n5 = document.getElementById("b5");
+    let n6 = document.getElementById("b6");
+    let n7 = document.getElementById("b7");
+    let n8 = document.getElementById("b8");
+    let n9 = document.getElementById("b9");
+    
+
+
+function limpaTela() {
+    document.getElementById("numero-tela").innerHTML = "0";
+};
+function printNunber0() {
+    let n0 = document.getElementById("b0").value;
+    document.getElementById("numero-tela").innerHTML = n0;
+};
+function printNunber1() {
+    let n1 = document.getElementById("b1").value;
+    document.getElementById("numero-tela").innerHTML = n1;
+};
+function printNunber2() {
+    document.getElementById("numero-tela").innerHTML = n2;
+};
+function printNunber3() {
+    document.getElementById("numero-tela").innerHTML = 3;
+};
+function printNunber4() {
+    document.getElementById("numero-tela").innerHTML = 4;
+};
+function printNunber5() {
+    document.getElementById("numero-tela").innerHTML = 5;
+};
+function printNunber6() {
+    document.getElementById("numero-tela").innerHTML = 6;
+};
+function printNunber7() {
+    document.getElementById("numero-tela").innerHTML = 7;
+};
+function printNunber8() {
+    document.getElementById("numero-tela").innerHTML = 8;
+};
+function printNunber9() {
+    document.getElementById("numero-tela").innerHTML = 9;
+};
+
